de1e1b888e80009fc1c284a27d6e11141b2e7ed1
Utsav Aggarwal <utsavaggarwal@Utsavs-MacBook-Air.local>
2025-08-24 02:11:59 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+        <img width={150} src={assets.logo} alt="" />
+        <div className='flex gap-2.5'>
+            <img width={35} src={assets.facebook_icon} alt="" />
+            <img width={35} src={assets.twitter_icon} alt="" />
+            <img width={35} src={assets.instagram_icon} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
